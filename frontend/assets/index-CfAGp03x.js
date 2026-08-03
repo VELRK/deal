1da94bb53456713d9f2594a0591a1bc063e8a330
idstechdev <idstechdev@gmail.com>
2026-08-03 15:47:28 +0530

    Update frontend build assets

---
+++ frontend/assets/index-CfAGp03x.js
@@ -0,0 +1,1 @@
+import{M as r}from"./index-y4BVA3aJ.js";var a=r();export{a as r};
---
+++ frontend/assets/productViewStore-CCna-ar9.js
@@ -0,0 +1,1 @@
+import{J as r}from"./index-y4BVA3aJ.js";const u=r(e=>({productKey:null,openView:o=>e({productKey:o}),closeView:()=>e({productKey:null})}));export{u};
